d9fba4d7491a6e393f4219d88319286127df3eb2
stschoelzel <info@stschoelzel.de>
2021-04-19 22:18:18 +0200

    components & utilityclass

---
+++ src/components/Static.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const Static = () => {
+  return (
+    <div class="flex flex-col items-center justify-center p-20">
+      <div class="staticelement">
+        <a href="https://infocafe.org/">website</a>
+      </div>
+      <div class="staticelement">
+        <a href="https://www.instagram.com/infocafeni/">instagram</a>
+      </div>
+      <div class="staticelement">
+        <a href="https://www.youtube.com/infocafe">youtube</a>
+      </div>
+      <div class="staticelement">
+        <a href="https://www.twitch.tv/infocafe">twitter</a>
+      </div>
+      <div class="staticelement">
+        <a href="https://twitter.com/Infocafe">twitch</a>
+      </div>
+    </div>
+  );
+};
