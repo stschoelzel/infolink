75cb60b090159104b544aed51855002bdfea3e44
stschoelzel <info@stschoelzel.de>
2021-04-19 20:47:12 +0200

    joa ne

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg'; //faviocon machen finden bla
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>  
+      <div class="flex flex-col items-center justify-center p-20">     
+        <div class="bg-blue-300 text-gray-800 font-bold rounded-md p-2 w-1/5 text-center hover:bg-blue-100"><a href="https://infocafe.org/">website</a></div>
+        <div class="bg-blue-300 text-gray-800 font-bold rounded-md p-2 w-1/5 text-center hover:bg-blue-100"><a href="https://www.instagram.com/infocafeni/">instagram</a></div>
+        <div class="bg-blue-300 text-gray-800 font-bold rounded-md p-2 w-1/5 text-center hover:bg-blue-100"><a href="https://www.youtube.com/infocafe">youtube</a></div>
+        <div class="bg-blue-300 text-gray-800 font-bold rounded-md p-2 w-1/5 text-center hover:bg-blue-100"><a href="https://www.twitch.tv/infocafe">twitter</a></div>
+        <div class="bg-blue-300 text-gray-800 font-bold rounded-md p-2 w-1/5 text-center hover:bg-blue-100"><a href="https://twitter.com/Infocafe">twitch</a></div>
+      </div>
+
+      <div class="flex flex-wrap m-2">
+        
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center bg-blue-100 p-4 rounded-md">
+            <img class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://infocafe.org/wp-content/uploads/2020/08/twitch.png"></img>
+            <div class="flex-grow">
+              <h2 class="text-gray-800 font-bold">Club Digital – Die Daten!</h2>
+              <p class="text-gray-600">website</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center bg-blue-100 p-4 rounded-md">
+            <img class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://infocafe.org/wp-content/uploads/2020/08/twitch.png"></img>
+            <div class="flex-grow">
+              <h2 class="text-gray-800 font-bold">Club Digital – Die Daten!</h2>
+              <p class="text-gray-600">website</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center bg-blue-100 p-4 rounded-md">
+            <img class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://infocafe.org/wp-content/uploads/2020/08/twitch.png"></img>
+            <div class="flex-grow">
+              <h2 class="text-gray-800 font-bold">Club Digital – Die Daten!</h2>
+              <p class="text-gray-600">website</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center bg-blue-100 p-4 rounded-md">
+            <img class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://infocafe.org/wp-content/uploads/2020/08/twitch.png"></img>
+            <div class="flex-grow">
+              <h2 class="text-gray-800 font-bold">Club Digital – Die Daten!</h2>
+              <p class="text-gray-600">website</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    
     </div>
-  );
+
+
+    );
 }
 
 export default App;
