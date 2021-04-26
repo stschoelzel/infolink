e767505c0178ae72ce7fd5d644ed41d00022e482
stschoelzel <info@stschoelzel.de>
2021-04-27 00:14:38 +0200

    New static links + apollo?

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 // import logo from './logo.svg'; //faviocon machen finden bla
 
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+
 import { Dynamic } from "./components/Dynamic";
 import { Static } from "./components/Static";
+
+const link = createHttpLink({
+  uri: "https://infocafe.org/graphql",
+});
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
--- src/components/Static.js
+++ src/components/Static.js
@@ -2,21 +2,46 @@ import React from "react";
 
 export const Static = () => {
   return (
-    <div class="flex flex-col flex-nowrap items-center justify-center p-10 space-y-2">
-      <div class="staticelement">
-        <a href="https://infocafe.org/">website</a>
+    <div class="flex flex-col flex-nowrap items-center justify-center pt-7 pb-3">
+      <div class="h-8 w-40 relative cursor-pointer mb-5">
+        <div class="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
+        <div class="absolute inset-0 transform  hover:scale-90 transition duration-300">
+          <div class="h-full w-full bg-blue-300 text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
+            <a href="https://infocafe.org/">website</a>
+          </div>
+        </div>
       </div>
-      <div class="staticelement">
-        <a href="https://www.instagram.com/infocafeni/">instagram</a>
+      <div class="h-8 w-40 relative cursor-pointer mb-5">
+        <div class="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
+        <div class="absolute inset-0 transform  hover:scale-90 transition duration-300">
+          <div class="h-full w-full bg-blue-300 text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
+            <a href="https://www.instagram.com/infocafeni/">instagram</a>
+          </div>
+        </div>
       </div>
-      <div class="staticelement">
-        <a href="https://www.youtube.com/infocafe">youtube</a>
+      <div class="h-8 w-40 relative cursor-pointer mb-5">
+        <div class="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
+        <div class="absolute inset-0 transform  hover:scale-90 transition duration-300">
+          <div class="h-full w-full bg-blue-300  text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
+            <a href="https://www.youtube.com/infocafe">youtube</a>
+          </div>
+        </div>
       </div>
-      <div class="staticelement">
-        <a href="https://www.twitch.tv/infocafe">twitter</a>
+      <div class="h-8 w-40 relative cursor-pointer mb-5">
+        <div class="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
+        <div class="absolute inset-0 transform  hover:scale-90 transition duration-300">
+          <div class="h-full w-full bg-blue-300 text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
+            <a href="https://www.twitch.tv/infocafe">twitter</a>
+          </div>
+        </div>
       </div>
-      <div class="staticelement">
-        <a href="https://twitter.com/Infocafe">twitch</a>
+      <div class="h-8 w-40 relative cursor-pointer mb-5">
+        <div class="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
+        <div class="absolute inset-0 transform  hover:scale-90 transition duration-300">
+          <div class="h-full w-full bg-blue-300 text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
+            <a href="https://twitter.com/Infocafe">twitch</a>
+          </div>
+        </div>
       </div>
     </div>
   );
