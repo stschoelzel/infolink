17e286c5f0b7c64fbdd555d790155fadb7cde858
stschoelzel <info@stschoelzel.de>
2021-05-12 22:52:06 +0200

    Logos und so

--- src/components/Dynamic.js
+++ src/components/Dynamic.js
@@ -7,9 +7,14 @@ const INFOLINK = gql`
     infolinks {
       edges {
         node {
+          title
+          id
           infolinkMeta {
             what
-            where
+            where {
+              mediaItemUrl
+            }
+            url
             info
           }
         }
@@ -18,18 +23,18 @@ const INFOLINK = gql`
   }
 `;
 
-const logoUrls = {
-  Website: "https://infocafe.org/wp-content/uploads/2020/08/twitch.png",
-  Twitch: "https://infocafe.org/wp-content/uploads/2020/08/twitch.png",
-  Youtube: "https://infocafe.org/wp-content/uploads/2020/08/twitch.png",
-};
+// const logoUrls = {
+//   Website: "https://infocafe.org/wp-content/uploads/2020/08/twitch.png",
+//   Twitch: "https://infocafe.org/wp-content/uploads/2020/08/twitch.png",
+//   Youtube: "https://infocafe.org/wp-content/uploads/2020/08/twitch.png",
+// };
 
 export const Dynamic = () => {
   const { data } = useQuery(INFOLINK);
   const infolinks = data ? formatData(data) : [];
 
-  return infolinks.map(({ what, where, info }) => (
-    <Infolink what={what} where={where} info={info} />
+  return infolinks.map(({ what, where, info, url }) => (
+    <Infolink what={what} where={where} info={info} url={url} />
   ));
 };
 
@@ -37,20 +42,25 @@ const formatData = (data) => {
   return data.infolinks.edges.map((element) => element.node.infolinkMeta);
 };
 
-const Infolink = ({ where, what, info }) => {
+const Infolink = ({ where, what, info, url }) => {
   return (
-    <div className="flex flex-wrap m-2">
+    <div className="flex flex-wrap m-2 items-center justify-center ">
       <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div className="h-full flex items-center bg-blue-100 p-4 rounded-md">
-          <img
-            className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-            src={logoUrls[where]}
-          ></img>
-          <div className="flex-grow">
-            <h2 className="text-gray-800 font-bold">{what}</h2>
-            <p className="text-gray-600">{info}</p>
+        <a href={url}>
+          <div className="h-full flex items-center bg-blue-100 p-4 rounded-md">
+            <img
+              className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+              src={
+                where?.mediaItemUrl ||
+                "https://infocafe.org/wp-content/uploads/2020/10/cropped-cropped-logo_10-2011-1-2.jpg"
+              }
+            ></img>
+            <div className="flex-grow">
+              <h2 className="text-gray-800 font-bold">{what}</h2>
+              <p className="text-gray-600">{info}</p>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
     </div>
   );
--- src/components/Static.js
+++ src/components/Static.js
@@ -3,44 +3,22 @@ import React from "react";
 export const Static = () => {
   return (
     <div className="flex flex-col flex-nowrap items-center justify-center pt-7 pb-3">
-      <div className="h-8 w-40 relative cursor-pointer mb-5">
-        <div className="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
-        <div className="absolute inset-0 transform  hover:scale-90 transition duration-300">
-          <div className="h-full w-full bg-blue-300 text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
-            <a href="https://infocafe.org/">website</a>
-          </div>
-        </div>
-      </div>
-      <div className="h-8 w-40 relative cursor-pointer mb-5">
-        <div className="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
-        <div className="absolute inset-0 transform  hover:scale-90 transition duration-300">
-          <div className="h-full w-full bg-blue-300 text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
-            <a href="https://www.instagram.com/infocafeni/">instagram</a>
-          </div>
-        </div>
-      </div>
-      <div className="h-8 w-40 relative cursor-pointer mb-5">
-        <div className="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
-        <div className="absolute inset-0 transform  hover:scale-90 transition duration-300">
-          <div className="h-full w-full bg-blue-300  text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
-            <a href="https://www.youtube.com/infocafe">youtube</a>
-          </div>
-        </div>
-      </div>
-      <div className="h-8 w-40 relative cursor-pointer mb-5">
-        <div className="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
-        <div className="absolute inset-0 transform  hover:scale-90 transition duration-300">
-          <div className="h-full w-full bg-blue-300 text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
-            <a href="https://www.twitch.tv/infocafe">twitter</a>
-          </div>
-        </div>
-      </div>
-      <div className="h-8 w-40 relative cursor-pointer mb-5">
-        <div className="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
-        <div className="absolute inset-0 transform  hover:scale-90 transition duration-300">
-          <div className="h-full w-full bg-blue-300 text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
-            <a href="https://twitter.com/Infocafe">twitch</a>
-          </div>
+      <Button url="https://twitter.com/Infocafe" label="twitch" />
+      <Button url="https://infocafe.org" label="website" />
+      <Button url="https://www.instagram.com/infocafeni" label="instagram" />
+      <Button url="https://www.youtube.com/infocafe" label="youtube" />
+      <Button url="https://twitter.com/Infocafe" label="twitter" />
+    </div>
+  );
+};
+
+const Button = ({ url, label }) => {
+  return (
+    <div className="h-8 w-40 relative cursor-pointer mb-5">
+      <div className="absolute inset-0 bg-blue-100 rounded-md shadow-2xl"></div>
+      <div className="absolute inset-0 transform  hover:scale-90 transition duration-300">
+        <div className="h-full w-full bg-blue-300 text-gray-800 text-base text-center font-bold rounded-md pt-1 shadow-2xl">
+          <a href={url}>{label}</a>
         </div>
       </div>
     </div>
